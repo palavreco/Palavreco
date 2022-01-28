a7267e0f8e7d3b4f1d96d8dc39b90d3119278a05
Vitor Lopes <milagre.vitor@gmail.com>
2022-01-28 19:19:49 +0000

    fix: add semicolons and delete unnecessary import

--- src/update-commands.js
+++ src/update-commands.js
@@ -1,5 +1,3 @@
-// Importa a classe responsável pela criação dos comandos
-const { SlashCommandBuilder } = require('@discordjs/builders');
 // Importa a classe que gerencia o envio de JSONs para o Discord
 const { REST } = require('@discordjs/rest');
 // Importa a constante que é usada para identificar o caminho que os comandos são mandados para o Discord
@@ -7,15 +5,14 @@ const { Routes } = require('discord-api-types/v9');
 // Importa as informações frágeis do bot
 const { clientId, guildId, token } = require('./secrets.json');
 // Importa a função usada para ler os diretórios
-const { readdirSync } = require('fs')
+const { readdirSync } = require('fs');
 
 // Array de comandos do bot, que depois é passado para JSON
-const commandsPath = require('path').join(__dirname, 'commands')
-const commands = []
+const commandsPath = require('path').join(__dirname, 'commands');
+const commands = [];
 readdirSync(commandsPath).forEach(file => {
-	commands.push(require(`./commands/${file}`).data)
-})
-
+	commands.push(require(`./commands/${file}`).data);
+});
 
 // Cria uma nova instância de REST
 const rest = new REST({ version: '9' }).setToken(token);
@@ -23,4 +20,4 @@ const rest = new REST({ version: '9' }).setToken(token);
 // Envia o array de comandos para o Discord, tornando os comandos disponíveis para uso
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Comandos atualizados com sucesso!'))
-	.catch(console.error);
+	.catch((error) => console.error(error));
