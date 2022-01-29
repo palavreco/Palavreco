19d1343fbdc6ba18281252c05bfa33da974915ce
Eduardo Rdrigues <eduardorodrigues011@icloud.comm>
2022-01-28 21:54:10 -0300

    fix: inconsistency in emoji conversionfixing rate

--- src/commands/adivinhar.js
+++ src/commands/adivinhar.js
@@ -18,20 +18,45 @@ async function convertMessageContentToEmojis(interaction, content, correctWord) 
 	const contentArray = content.split('');
 	const correctWordArray = correctWord.split('');
 
-	let finalMessage = '';
+	const usedLetters = [];
+
+	const finalMessage = {
+		'1': '',
+		'2': '',
+		'3': '',
+		'4': '',
+		'5': '',
+	};
+
 	for (let i = 0; i < contentArray.length; i++) {
 		if (contentArray[i] === correctWordArray[i]) {
-			finalMessage += letter['green'][correctWordArray[i]];
-		}
-		else if (correctWordArray.includes(contentArray[i])) {
-			finalMessage += letter['yellow'][contentArray[i]];
-		}
-		else {
-			finalMessage += letter['gray'][contentArray[i]];
+			usedLetters.push(contentArray[i]);
+			finalMessage[`${i + 1}`] = letter['green'][correctWordArray[i]];
 		}
 	}
 
-	await interaction.editReply(finalMessage);
+	for (let i = 0; i < contentArray.length; i++) {
+
+		if (correctWordArray.includes(contentArray[i]) && contentArray[i] !== correctWordArray[i]) {
+			usedLetters.push(contentArray[i]);
+
+			const caracterCountCorrect = correctWordArray.filter(car => car === contentArray[i]);
+			const caracterCountContent = usedLetters.filter(car => car === contentArray[i]);
+
+			if (caracterCountContent.length > caracterCountCorrect.length) {finalMessage[`${i + 1}`] = letter['gray'][contentArray[i]];}
+			else {finalMessage[`${i + 1}`] = letter['yellow'][contentArray[i]];}
+		}
+		else if (contentArray[i] !== correctWordArray[i]) {
+			usedLetters.push(contentArray[i]);
+
+			finalMessage[`${i + 1}`] = letter['gray'][contentArray[i]];
+		}
+	}
+
+
+	const finalReply = `${finalMessage['1']} ${finalMessage['2']} ${finalMessage['3']} ${finalMessage['4']} ${finalMessage['5']}`;
+
+	await interaction.editReply(finalReply);
 }
 
 module.exports = {
@@ -41,7 +66,7 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.reply('Adivinhe o **WORDLE** de hoje! :eyes:');
 
-		const correctWord = 'teste'; // Apenas para teste, futuramente será retirado
+		const correctWord = 'vasco'; // Apenas para teste, futuramente será retirado
 
 		const receivedMessage = await awaitMessage(interaction);
 
@@ -50,7 +75,7 @@ module.exports = {
 			await receivedMessage.message.delete();
 		}
 		else {
-			await convertMessageContentToEmojis(interaction, receivedMessage.content, correctWord);
+			await convertMessageContentToEmojis(interaction, receivedMessage.content.toLowerCase(), correctWord.toLowerCase());
 			await receivedMessage.message.delete();
 		}
 	},
