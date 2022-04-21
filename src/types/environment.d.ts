3a04a7aa5599549bbcd2a6ea11f34d6abd72f32c
Vitor Lopes <milagre.vitor@gmail.com>
2022-04-21 21:53:46 +0100

    feat: add `DATABASE_URL` and `GUILD_UPDATE_CHANNEL`

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,11 +1,13 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      DATABASE_URL: string;
       TOKEN: string,
       CLIENT_ID: string,
       GUILD_ID: string
       SUG_CHANNEL: string,
       BUG_CHANNEL: string,
+      GUILD_UPDATE_CHANNEL: string;
     }
   }
 }
