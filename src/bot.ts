3e042b8e8b70bbb3271d7e9a26d4b22d7b7e8beb
Vitor <milagre.vitor@gmail.com>
2022-04-25 17:26:31 +0100

    feat: add logs & activity

--- src/bot.ts
+++ src/bot.ts
@@ -1,16 +1,61 @@
+/* eslint-disable no-trailing-spaces */
 import fs from 'node:fs';
 import dotenv from 'dotenv';
-import { Client, Collection } from 'discord.js';
+import { ActivityOptions, Client, Collection, Guild, MessageEmbed, TextChannel } from 'discord.js';
 import { Command } from './interfaces/Command';
-import { setUp } from './database';
+import { newDay, setUp } from './database';
 import { log } from './utils/log';
+import { runAtMidnight } from './utils/runner';
 dotenv.config();
 
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS'] });
 
 client.once('ready', () => {
 	log('Client is ready', 'BOT', 'green');
-	setUp();
+
+	client.user!.setActivity({ type: 'PLAYING', name: '/adivinhar' });
+
+	const ac: ActivityOptions[] = [
+		{ name: `em ${client.guilds.cache.size} servidores`, type: 'PLAYING' },
+		{ name: '/adivinhar', type: 'PLAYING' },
+		{ name: 'suas tentativas...', type: 'WATCHING' },
+	];
+	setInterval(() => {
+		const activity = ac[Math.floor(Math.random() * ac.length)];
+		client.user!.setActivity({ name: activity.name, type: activity.type });
+	}, 900_000);
+});
+
+const cLog = client.channels.cache.get(process.env.GUILD_UPDATE_CHANNEL) as TextChannel;
+
+client.on('guildCreate', async (guild: Guild) => {
+	const { createdTimestamp, ownerId, memberCount, name, id } = guild;
+
+	const owner = await guild.fetchOwner().then(o => o.user.tag);
+	const embed = new MessageEmbed()
+		.setAuthor({ name: `${name} (${id})` }).setTitle('Novo servidor!')
+		.addFields(
+			{ name: 'Dono', value: `\`${owner}\` (${ownerId})`, inline: true },
+			{ name: 'Membros', value: `${memberCount}`, inline: true },
+			{ name: 'Criado em', value: `<t:${Math.floor(createdTimestamp / 1000)}>`, inline: true },
+		).setFooter({ text: `Agora estou em ${client.guilds.cache.size} servidores!` }).setColor('#2f3136');
+
+	cLog.send({ embeds: [embed] });
+	log(`Joined in ${name} (${id})`, 'BOT', 'blue');
+});
+
+client.on('guildDelete', async (guild: Guild) => {
+	const { ownerId, name, id } = guild;
+
+	const owner = await guild.fetchOwner().then(o => o.user.tag);
+	const embed = new MessageEmbed()
+		.setAuthor({ name: `${name} (${id})` }).setTitle('Saí de um servidor :(')
+		.addFields(
+			{ name: 'Dono', value: `\`${owner}\` (${ownerId})`, inline: true },
+		).setFooter({ text: `Agora estou em ${client.guilds.cache.size} servidores!` }).setColor('#2f3136');
+
+	cLog.send({ embeds: [embed] });
+	log(`Leaved from ${name} (${id})`, 'BOT', 'red');
 });
 
 const botCmds: Collection<string, Command> = new Collection();
@@ -34,3 +79,9 @@ client.on('interactionCreate', i => {
 });
 
 client.login(process.env.TOKEN);
+setUp();
+
+runAtMidnight(() => {
+	newDay();
+	log('New word & users reseted!', 'DB', 'purple');
+});
