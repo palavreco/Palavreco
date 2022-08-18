cf1ae17c4b0d63497afd24ee576bd6bafd546f11
Vitor <milagre.vitor@gmail.com>
2022-08-18 20:25:49 +0100

    chore: things

--- src/utils/runner.ts
+++ src/utils/runner.ts
@@ -9,8 +9,6 @@ export function runAtEndOf(endOf: 'day' | 'month', callback: () => void) {
 	const time = +dayjs().tz('America/Sao_Paulo').endOf(endOf) + 1;
 	const msUntilTime = time - +dayjs();
 
-	console.log(`Running at ${new Date(time)} ${callback}`);
-
 	setTimeout(() => {
 		callback();
 		runAtEndOf(endOf, callback);
