b27f90a2985f1c09dc8b00677f29b3df458ec8ed
Leonardo <kaigo.dev@gmail.com>
2022-09-02 19:05:50 -0300

    style: fix typo and style if

--- src/utils/bisect.ts
+++ src/utils/bisect.ts
@@ -3,11 +3,10 @@ export function bisect<T>(array: T[], item: T): number {
 	let mid;
 	let high = array.length;
 	while (low < high) {
-		mid = (low + high) >>> 1; // faster version of Math.floor((lo + hi) / 2)
+		mid = (low + high) >>> 1; // faster version of Math.floor((low + high) / 2)
 		if (array[mid] < item) {
 			low = mid + 1;
-		}
-		else {
+		} else {
 			high = mid;
 		}
 	}
