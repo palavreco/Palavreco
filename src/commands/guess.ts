dfd8f4cdc1ad9c20bb0c5a1afe4702fefac69719
Vitor <milagre.vitor@gmail.com>
2022-09-19 20:51:08 +0100

    chore: merge main

--- src/commands/guess.ts
+++ src/commands/guess.ts
@@ -36,7 +36,7 @@ export default class Guess implements Command {
 
 	dev = false;
 
-	permissions: PermissionString[] = ['MANAGE_MESSAGES', 'VIEW_CHANNEL'];
+	permissions: PermissionString[] = ['MANAGE_MESSAGES', 'VIEW_CHANNEL', 'USE_EXTERNAL_EMOJIS'];
 
 	async execute(interaction: CommandInteraction) {
 		if (await verifyWord()) {
